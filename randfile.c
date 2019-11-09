0e89485cd88a66be6b6240629240b99b4961b441
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-09 05:16:00 -0500

    print generated array

--- randfile.c
+++ randfile.c
@@ -3,6 +3,7 @@
 
 #include<fcntl.h>
 #include<errno.h>
+#include<unistd.h>
 
 #include<string.h>
 
@@ -13,6 +14,21 @@ int main() {
     printf("error number %d: %s\n",errno,strerror(errno));
   }else {
     printf("successful file access!\n");
+    int ary[10];
+    int status;
+    status = read(fd,ary, 10 * sizeof(int) );
+    if(status == 10 * sizeof(int) ) {
+      printf("successful read from rand!\n");
+      short i;
+      printf("[ ");
+      for(i = 0; i < 10; i++) {
+	printf("%d ",ary[i]);
+      }
+      printf("]\n");
+    } else {
+      if(status >= 0) printf("Reached end of file, only read %d bytes\n",status);
+      else            printf("error number %d: %s\n",errno,strerror(errno));
+    }
   }
   return 0;
 }
